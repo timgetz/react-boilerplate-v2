5514345e83cf569e121003a111885c54ac2af9aa
Tim Getz <timgetz@gmail.com>
2016-11-07 19:55:23 -0500

    changed default repository

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -85,7 +85,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
                 <Input
                   id="username"
                   type="text"
-                  placeholder="mxstbr"
+                  placeholder="timgetz"
                   value={this.props.username}
                   onChange={this.props.onChangeUsername}
                 />
